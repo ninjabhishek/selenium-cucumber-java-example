b80168231419a79805cf6ce768dac8e94cc75c5b
abhishek-anand13 <anandabhishek58@gmail.com>
2023-08-30 16:29:25 +0530

    init

---
+++ src/test/java/Step.java
@@ -0,0 +1,13 @@
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Step {
+
+    private final WebDriver driver = new ChromeDriver();
+
+    @Given("User navigates to Gmail page")
+    public void login() {
+        driver.get("https://www.gmail.com");
+    }
+}
---
+++ src/test/resources/steps.feature
@@ -0,0 +1,5 @@
+Feature: Gmail login
+
+Scenario: Login scenario test for Gmail
+
+Given User navigates to Gmail page
